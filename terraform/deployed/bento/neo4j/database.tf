740329f9c7dbd60e03a400e08c37662a7cc8891f
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-03-20 19:23:58 -0400

    fixed yum issue with amz-linux

--- terraform/deployed/bento/neo4j/database.tf
+++ terraform/deployed/bento/neo4j/database.tf
@@ -148,7 +148,7 @@ mainSteps:
     - pip install --upgrade "pip < 21.0"
     - pip install ansible==2.8.0 boto boto3 botocore
     - git clone https://github.com/CBIIT/icdc-devops
-    - cd icdc-devops/ansible && git checkout master
+    - cd icdc-devops/ansible && git checkout develop
     - ansible-playbook community-neo4j.yml
     - systemctl restart neo4j
 DOC
