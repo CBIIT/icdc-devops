7c850f5a81c76a29854e8bff1b224041e5e7825d
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-03-30 20:11:50 -0400

    added fargate as module

--- terraform/modules/fargate/route53.tf
+++ terraform/modules/fargate/route53.tf
@@ -7,7 +7,7 @@ resource "aws_route53_record" "dns_record" {
   count = var.create_dns_record ? 1 : 0
   name = var.app_sub_domain
   type = "A"
-  zone_id = data.aws_route53_zone.zone.zone_id
+  zone_id = data.aws_route53_zone.zone[count.index].zone_id
   alias {
     evaluate_target_health = false
     name = aws_lb.alb.dns_name
