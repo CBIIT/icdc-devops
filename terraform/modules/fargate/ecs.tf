93b36348b161d6e7df87e36ce445cb17d73ec43c
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-03-30 21:49:40 -0400

    reworking fargate

--- terraform/modules/fargate/ecs.tf
+++ terraform/modules/fargate/ecs.tf
@@ -57,19 +57,21 @@ resource "aws_ecs_service" "service" {
 
 #create ecs cluster
 resource "aws_appautoscaling_target" "microservice_autoscaling_target" {
+  for_each = var.microservices
   max_capacity       = 5
   min_capacity       = 1
-  resource_id        = "service/${aws_ecs_cluster.ecs_cluster.name}/${aws_ecs_service.service.name}"
+  resource_id        = "service/${aws_ecs_cluster.ecs_cluster.name}/${aws_ecs_service.service[each.key].name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
 }
 
 resource "aws_appautoscaling_policy" "microservice_autoscaling_cpu" {
+  for_each = var.microservices
   name               = "cpu-autoscaling"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.microservice_autoscaling_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.microservice_autoscaling_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.microservice_autoscaling_target.service_namespace
+  resource_id        = aws_appautoscaling_target.microservice_autoscaling_target[each.key].resource_id
+  scalable_dimension = aws_appautoscaling_target.microservice_autoscaling_target[each.key].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.microservice_autoscaling_target[each.key].service_namespace
 
   target_tracking_scaling_policy_configuration {
     predefined_metric_specification {
