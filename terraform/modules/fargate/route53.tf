e99eb60a3ce6f4ed3d14d7a4b778f10bd0f4f08c
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-03-30 23:14:16 -0400

    changed ecs

--- terraform/modules/fargate/route53.tf
+++ terraform/modules/fargate/route53.tf
@@ -5,7 +5,7 @@ data "aws_route53_zone" "zone" {
 
 resource "aws_route53_record" "dns_record" {
   count = var.create_dns_record ? 1 : 0
-  name = var.app_sub_domain
+  name = "${var.app_sub_domain}-${var.env}"
   type = "A"
   zone_id = data.aws_route53_zone.zone[count.index].zone_id
   alias {
