3b352ca33b1f25dbde4939c8fdd932f3d7e316ae
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-04-17 23:03:59 -0400

    unified the cloud platform

--- terraform/deployed/bento/fargate/main.tf
+++ terraform/deployed/bento/fargate/main.tf
@@ -1,6 +1,6 @@
 locals {
   nih_ip_cidrs =  terraform.workspace == "prod" || terraform.workspace == "stage" ? ["0.0.0.0/0"]: [ "129.43.0.0/16" , "137.187.0.0/16" , "10.128.0.0/9" , "165.112.0.0/16" , "156.40.0.0/16" , "10.208.0.0/21" , "128.231.0.0/16" , "130.14.0.0/16" , "157.98.0.0/16" , "10.133.0.0/16" ]
-  all_ips      =  var.cloud_platform == "leidos" ? ["0.0.0.0/0"] : nih_ip_cidrs
+  all_ips      =  var.cloud_platform == "leidos" ? ["0.0.0.0/0"] : local.nih_ip_cidrs
   alb_subnets = terraform.workspace == "prod" || terraform.workspace == "stage"  || var.cloud_platform == "leidos" ? var.public_subnet_ids : var.private_subnet_ids
   app_url =  terraform.workspace == "prod" ? "${var.app_sub_domain}.${var.domain_name}" : "${var.app_sub_domain}-${terraform.workspace}.${var.domain_name}"
   alb_allowed_ip_range = terraform.workspace == "prod" || terraform.workspace == "stage" ?  local.all_ips : local.nih_ip_cidrs
