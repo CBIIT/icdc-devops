b2ffe406e2ce11c560b660e3c57b447678c2eaf7
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-07-26 12:08:54 -0400

    added qa env for aurora

--- terraform/deployed/bento/aurora/outputs.tf
+++ terraform/deployed/bento/aurora/outputs.tf
@@ -1,3 +1,6 @@
 output "cluster_endpoint" {
   value = module.aurora.cluster_endpoint
 }
+output "db_password" {
+  value = module.aurora.db_password
+}
--- terraform/modules/aurora/outputs.tf
+++ terraform/modules/aurora/outputs.tf
@@ -2,3 +2,6 @@
 output "cluster_endpoint" {
   value = aws_rds_cluster.rds.endpoint
 }
+output "db_password" {
+  value = random_password.master_password.result
+}
