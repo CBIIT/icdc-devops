30d948eaec1745b1afdf8938ef10dd4e3e4a39ff
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-07-29 00:04:23 -0400

    adding gmb tfvars

--- infra/terraform/bento/workspace/scripts/install.sh
+++ infra/terraform/bento/workspace/scripts/install.sh
@@ -2,3 +2,8 @@
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
+mkdir workspace || true
+cd workspace
+git clone https://github.com/CBIIT/icdc-devops.git
+cd icdc-devops && git checkout master
+cd infra/terraform/bento
