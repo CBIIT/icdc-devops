57fb89813b873d6a267e3a88d30835c4b0b99b8c
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-08-08 11:38:34 -0400

    added instance profle

--- infra/terraform/bento/instance-profile.tf
+++ infra/terraform/bento/instance-profile.tf
@@ -1,25 +1,25 @@
 resource "aws_iam_instance_profile" "integration_server" {
-  count = var.create_instance_profile
+  count = var.create_instance_profile ? 1 : 0
   name = local.integration_server_profile_name
   role = aws_iam_role.integration_server.name
 }
 
 resource "aws_iam_role" "integration_server" {
-  count = var.create_instance_profile
+  count = var.create_instance_profile ? 1 : 0
   name                 = local.integration_server_profile_name
   assume_role_policy   = data.aws_iam_policy_document.integration_server_assume_role.json
   permissions_boundary = terraform.workspace == "prod" ? null : local.permissions_boundary
 }
 
 resource "aws_iam_policy" "integration_server" {
-  count = var.create_instance_profile
+  count = var.create_instance_profile ? 1 : 0
   name        = "${local.integration_server_profile_name}-policy"
   description = "IAM Policy for the integration server host in this account"
   policy      = data.aws_iam_policy_document.integration_server_policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "integration_server" {
-  count = var.create_instance_profile
+  count = var.create_instance_profile ? 1 : 0
   role       = aws_iam_role.integration_server[count.index].name
   policy_arn = aws_iam_policy.integration_server[count.index].arn
 }
