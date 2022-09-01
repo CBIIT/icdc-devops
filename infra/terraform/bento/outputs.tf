265bcb5ced84143a17780dc2325c001f21dfb902
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-09-01 00:05:57 -0400

    added perf vars

--- infra/terraform/bento/outputs.tf
+++ infra/terraform/bento/outputs.tf
@@ -5,9 +5,9 @@
 //  value = module.opensearch.opensearch_endpoint
 //}
 output "cluster_endpoint" {
-  value = module.aurora.cluster_endpoint
+  value = module.aurora.*.cluster_endpoint
 }
 output "db_password" {
-  value = module.aurora.db_password
+  value = module.aurora.*.db_password
   sensitive = true
 }
