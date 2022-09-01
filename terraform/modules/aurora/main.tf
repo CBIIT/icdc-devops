c641eccd0ab24c0153a853be580802b5da12abe8
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-08-31 23:55:10 -0400

    added perf vars

--- terraform/modules/aurora/main.tf
+++ terraform/modules/aurora/main.tf
@@ -29,11 +29,11 @@ resource "aws_rds_cluster" "rds" {
   deletion_protection                 =  var.deletion_protection
   db_subnet_group_name                =  aws_db_subnet_group.subnet_group.name
   vpc_security_group_ids              =  [aws_security_group.rds.id]
+  kms_key_id                          =  data.aws_kms_alias.kms.id
   serverlessv2_scaling_configuration {
     max_capacity =  var.max_capacity
     min_capacity =  var.min_capacity
   }
-
   tags = var.tags
 
 }
