67bbc8248613d488c42e0db2972475056e34a32b
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-09-15 09:21:03 -0400

    remove unused module

--- infra/terraform/bento/main.tf
+++ infra/terraform/bento/main.tf
@@ -114,14 +114,14 @@ module "aurora" {
 }
 
 #cloudfront
-module "cloudfront" {
-  count = var.create_cloudfront ? 1 : 0
-  source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/cloudfront"
-  cloudfront_distribution_bucket_name = var.cloudfront_distribution_bucket_name
-  cloudfront_distribution_log_bucket_name = var.cloudfront_distribution_log_bucket_name
-  cloudfront_log_path_prefix_key = local.cloudfront_log_path_prefix
-  cloudfront_origin_access_identity_description = "cloudfront origin access identity for annotation files"
-  env = terraform.workspace
-  stack_name = var.stack_name
-  tags = var.tags
-}
+#module "cloudfront" {
+#  count = var.create_cloudfront ? 1 : 0
+#  source = "git::https://github.com/CBIIT/datacommons-devops.git//terraform/modules/cloudfront"
+#  cloudfront_distribution_bucket_name = var.cloudfront_distribution_bucket_name
+#  cloudfront_distribution_log_bucket_name = var.cloudfront_distribution_log_bucket_name
+#  cloudfront_log_path_prefix_key = local.cloudfront_log_path_prefix
+#  cloudfront_origin_access_identity_description = "cloudfront origin access identity for annotation files"
+#  env = terraform.workspace
+#  stack_name = var.stack_name
+#  tags = var.tags
+#}
