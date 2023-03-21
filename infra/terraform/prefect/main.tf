27b705bcfb1115fe954c392b92efb1b04c226e77
cloudprofessionals <mycloudprofessionals@gmail.com>
2023-03-21 13:25:01 -0400

    fixed typo in prefect vars

--- infra/terraform/prefect/main.tf
+++ infra/terraform/prefect/main.tf
@@ -66,3 +66,8 @@ module "aurora" {
   db_subnet_ids = var.db_subnet_ids
   database_name = var.database_name
 }
+
+resource "aws_s3_bucket_policy" "alb_bucket_policy" {
+  bucket = module.s3.bucket_id
+  policy = data.aws_iam_policy_document.s3_alb_policy.json
+}
